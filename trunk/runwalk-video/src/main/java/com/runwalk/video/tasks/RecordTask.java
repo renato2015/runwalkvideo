558c081219f6ed7e0500b999ec99d28ed7917dd6
jeroen_peelaerts <jeroen_peelaerts@hotmail.com>
2011-06-19 16:45:11 +0000

    -Removed imports and enhanced logging

--- trunk/runwalk-video/src/main/java/com/runwalk/video/tasks/RecordTask.java
+++ trunk/runwalk-video/src/main/java/com/runwalk/video/tasks/RecordTask.java
@@ -2,9 +2,6 @@ package com.runwalk.video.tasks;
 
 import java.io.File;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-
 import com.runwalk.video.dao.DaoService;
 import com.runwalk.video.entities.Analysis;
 import com.runwalk.video.entities.Recording;
@@ -43,7 +40,7 @@ public class RecordTask extends AbstractTask<Boolean, Void> {
 			File parentDir = videoFile.getParentFile();
 			if (!parentDir.exists()) {
 				boolean mkdirs = parentDir.mkdirs();
-				getLogger().debug("Directory creation result for " + parentDir.getName() + " is " + mkdirs);
+				getLogger().debug("Directory creation result for " + parentDir.getAbsolutePath() + " is " + mkdirs);
 			}
 			capturer.startRecording(recording, videoFile);
 		}
