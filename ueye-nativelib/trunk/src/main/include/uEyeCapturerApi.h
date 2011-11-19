d609afcf32cfd1ecccd845b96605f8ec218a64f0
jeroen_peelaerts@hotmail.com <jeroen_peelaerts@hotmail.com@6e040654-f594-11de-9e29-7d1cd92887ef>
2011-11-19 21:14:50 +0000

    -Fixed signature of GetCameraNames to avoid creating memory leaks

--- ueye-nativelib/trunk/src/main/include/uEyeCapturerApi.h
+++ ueye-nativelib/trunk/src/main/include/uEyeCapturerApi.h
@@ -20,13 +20,37 @@
 #define IS_RUNNING						0x0200
 #define SET_AVI_ID						0x0300
 #define SET_HWND						0x0400
+/*
+#ifndef UEYE_CAMERA_HANDLE_STRUCT
+#define UEYE_CAMERA_HANDLE_STRUCT
+typedef struct UEYE_CAMERA_HANDLE
+{
+  HIDS				hCam;
+  UEYE_CAMERA_DATA	ucd[1];
+};
+#endif //UEYE_CAMERA_HANDLE_STRUCT
 
+#ifndef UEYE_CAMERA_DATA_STRUCT
+#define UEYE_CAMERA_DATA_STRUCT
+typedef struct UEYE_CAMERA_DATA
+{
+	INT		nSizeX, nSizeY;
+	INT		lMemoryId;	// grabber memory - buffer ID
+	char*	pcImageMemory;// grabber memory - pointer to 
+	INT*	nMonitorId;
+	INT		nAviID;
+	BOOL    bRecording;
+	BOOL	bRunning;
+	CuEyeRenderThread* pRenderThread;
+};
+#endif //UEYE_CAMERA_DATA_STRUCT
+*/
 #ifdef __cplusplus
 extern "C" {
 #endif  /* __cplusplus */
 	INT WINAPI StartRecording(HIDS* m_hCam, const char* strFilePath);
 	INT WINAPI StopRecording(HIDS* m_hCam);
-	void WINAPI GetCameraNames(UEYE_CAMERA_LIST* cameraList);
+	INT WINAPI GetCameraNames(UEYE_CAMERA_LIST* cameraList);
 	INT WINAPI InitializeCamera(HIDS* m_hCam);
 	void WINAPI GetFrameDropInfo(HIDS* m_hCam, unsigned long* frameDropInfo);
 	INT WINAPI StartRunning(HIDS* m_hCam, const char* settingsFile, LPTSTR windowName, int* monitorId, void (WINAPI*OnWindowShowCallback)(BOOL), HWND windowHandle);
@@ -36,7 +60,6 @@ extern "C" {
 	void WINAPI SetWndVisibility(HIDS* hCam, BOOL visible);
 	BOOL WINAPI FilterDllMsg(LPMSG lpMsg);
 	void WINAPI ProcessDllIdle();
-
 #ifdef __cplusplus
 	}
 #endif
